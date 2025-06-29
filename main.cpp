2bd892bccd6f59c26320f1ef4c2352783c200b39
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-06-29 22:42:26 +0700

    Refactor previous main

--- main.cpp
+++ main.cpp
@@ -8,45 +8,63 @@ void log(const std::string& msg)
     std::cout << msg << std::endl;
 }
 
-int main()
+void setup_socket(int& server_fd, struct sockaddr_in& address)
 {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8000);
 
     int opt = 1;
-    log("setsockopt");
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    log("bind");
     bind(server_fd, (struct sockaddr*)&address, sizeof(address));
 
-    log("listen");
     listen(server_fd, SOMAXCONN);
+}
 
-    log("accept");
+void wait_for_client(int& server_fd, struct sockaddr_in& address, int& client_fd)
+{
     int addrlen = sizeof(address);
-    int client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
 
-    log("read");
-    char buffer[3000] = {0};
+    client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+}
+
+void read_request(int& client_fd, char* buffer)
+{
     read(client_fd, buffer, 3000);
+}
 
-    std::string response =
+void send_response(int& client_fd)
+{
+    const std::string response =
         "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/html\r\n"
         "Content-Length: 36\r\n"
         "\r\n"
         "<html><body>Hello world</body></html>";
 
-    log("send");
     send(client_fd, response.c_str(), response.size(), 0);
-    
-    log("close");
-    close(client_fd);
+}
 
+void serve_client(int& client_fd)
+{
+    char buffer[3000] = {0};
+
+    read_request(client_fd, buffer);
+    send_response(client_fd);
+    close(client_fd);
+}
+
+int main()
+{
+    int server_fd;
+    struct sockaddr_in address;
+    int client_fd;
+
+    setup_socket(server_fd, address);
+    wait_for_client(server_fd, address, client_fd);
+    serve_client(client_fd);
     return (EXIT_SUCCESS);
 }
